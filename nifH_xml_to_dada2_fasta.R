1c6e10192ca9d08fbbd6c28bb4f14812bcd0c203
moyn413 <moyn0001@e.ntu.edu.sg>
2020-07-24 09:36:22 +0800

    update

---
+++ nifH_xml_to_dada2_fasta.R
@@ -0,0 +1,12 @@
+library(XML)
+library(xml2)
+library(methods)
+library(tibble)
+library(stringr)
+library(plyr)
+library(dplyr)
+library(seqinr)
+
+
+setwd("/Users/molly/Dropbox/NitrogenFixation_Singapore/Molecular/nifHdatabases/")
+
